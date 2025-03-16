b50d6cb36f03c58bdae0a29fa068e3546fc95ed0
Katiuscia <katiusciabalia@gmail.com>
2025-03-16 17:58:11 +0100

    ultime modifiche al menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,55 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
-import Logo from "/assets/logo.png"; 
+import Logo from "/assets/logo.png";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   const [currentDate, setCurrentDate] = useState(getFormattedDate());
 
-
-  // Aggiorna lo stato quando cambia la dimensione dello schermo
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth > 768) {
-        setMenuOpen(false); // Chiude il menu se si torna su desktop
+        setMenuOpen(false);
         setIsMobile(false);
       } else {
         setIsMobile(true);
       }
     };
-
-
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  // Effetto per aggiornare la data ogni giorno
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentDate(getFormattedDate());
-    }, 86400000); // Aggiorna ogni 24 ore
-
+    }, 86400000);
     return () => clearInterval(interval);
   }, []);
 
+  function handleMenuToggle() {
+    setMenuOpen(!menuOpen);
+    setDropdownOpen(false);
+  }
+
+  function handleDropdownToggle(event) {
+    event.stopPropagation();
+    setDropdownOpen(!dropdownOpen);
+  }
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (!event.target.closest(".dropdown")) {
+        setDropdownOpen(false);
+      }
+    }
+    document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  }, []);
 
   return (
     <header className="nyt-header">
@@ -58,11 +73,9 @@ function Header() {
 
       {/* Logo + Menu Hamburger */}
       <div className="logo-section">
-      <img src={Logo} alt="New York Times Logo" width="150" />
-        
-        {/* Menu Hamburger visibile solo su mobile */}
+        <img src={Logo} alt="New York Times Logo" width="150" />
         {isMobile && (
-          <button className="hamburger-menu" onClick={() => setMenuOpen(!menuOpen)}>
+          <button className="hamburger-menu" onClick={handleMenuToggle}>
             {menuOpen ? "✖" : "☰"}
           </button>
         )}
@@ -88,7 +101,7 @@ function Header() {
           </div>
 
           {/* Navbar dentro il menu mobile */}
-
+        <div className="mobile-menu open">
           <nav className="custom-nav">
             <ul>
               <li><Link to="/">Home</Link></li>
@@ -99,22 +112,30 @@ function Header() {
           </nav>
 
           <nav className="nav-bar">
-          <ul>
-          <li><a href="https://www.nytimes.com/section/us">U.S. <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></a></li>
-          <li><a href="https://www.nytimes.com/section/world">World</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
-          <li><a href="https://www.nytimes.com/section/business">Business</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
-          <li><a href="https://www.nytimes.com/section/arts">Arts</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
-          <li><a href="https://www.nytimes.com/spotlight/lifestyle">Lifestyle</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
-          <li><a href="https://www.nytimes.com/section/opinion">Opinion</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
-          <li><a href="https://www.nytimes.com/spotlight/podcasts">Audio</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
-          <li><a href="https://www.nytimes.com/crosswords">Games</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
-          <li><a href="https://cooking.nytimes.com/">Cooking</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
-          <li><a href="https://www.nytimes.com/wirecutter/">Wirecutter</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
-          <li><a href="https://www.nytimes.com/athletic/uk/">The Athletic</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
-          </ul>
-          </nav>
-        </div>
-      )}
+              <div className="dropdown text-center">
+              <button className="btn btn-secondary dropdown-toggle" type="button" onClick={handleDropdownToggle}>
+                Global News
+              </button>
+              {dropdownOpen && (
+                <ul className="dropdown-menu show text-center">
+                <li><a href="https://www.nytimes.com/section/us">U.S. <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></a></li>
+                <li><a href="https://www.nytimes.com/section/world">World</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
+                <li><a href="https://www.nytimes.com/section/business">Business</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
+                <li><a href="https://www.nytimes.com/section/arts">Arts</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
+                <li><a href="https://www.nytimes.com/spotlight/lifestyle">Lifestyle</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
+                <li><a href="https://www.nytimes.com/section/opinion">Opinion</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
+                <li><a href="https://www.nytimes.com/spotlight/podcasts">Audio</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
+                <li><a href="https://www.nytimes.com/crosswords">Games</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
+                <li><a href="https://cooking.nytimes.com/">Cooking</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
+                <li><a href="https://www.nytimes.com/wirecutter/">Wirecutter</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
+                <li><a href="https://www.nytimes.com/athletic/uk/">The Athletic</a> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/></svg></li>
+                </ul>
+              )}
+              </div>
+            </nav>
+          </div>  
+      </div>
+    )}
 
       {/* Navbar visibile su desktop */}
       {!isMobile && (
