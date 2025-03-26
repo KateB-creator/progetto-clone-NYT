9fd12ecaf330aa459854222e25e71d68a4fe5f7c
Katiuscia <katiusciabalia@gmail.com>
2025-03-26 19:43:10 +0100

    aggiunta cartella footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css'; 
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2025 NYT Clone di Balia Katiuscia. All rights reserved.</p>
+        <ul>
+          <li><a href="https://kateb-creator.github.io/il-mio-sito-web/">About Me</a></li>
+          <li><a href="https://kateb-creator.github.io/il-mio-sito-web/#contact">Contact</a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+
